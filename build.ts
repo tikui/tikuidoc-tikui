99f0afeaac8a20f6c3452f9a44062c02e8ec8db4
Anthony Rey <gnuk@mailoo.org>
2024-07-25 09:51:57 +0200

    Update lint and format

--- build.ts
+++ build.ts
@@ -1,20 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 
-
 const DIST_LIB = path.resolve(__dirname, 'src', 'documentation', 'lib');
 const FONT = '@fontsource/montserrat';
 const MODULE_FONT = path.dirname(require.resolve(`${FONT}/package.json`));
 const DIST_LIB_FONT = path.resolve(DIST_LIB, FONT);
 
-
-const fontCopy = (file: string) => fs.cpSync(path.resolve(MODULE_FONT, file), path.resolve(DIST_LIB_FONT, file), {recursive: true});
+const fontCopy = (file: string) =>
+  fs.cpSync(path.resolve(MODULE_FONT, file), path.resolve(DIST_LIB_FONT, file), {
+    recursive: true,
+  });
 const fontCopyFiles = (...files: string[]) => files.forEach(fontCopy);
 
 console.log(`Reset dist lib folder at ${DIST_LIB}`);
 
-fs.rmSync(DIST_LIB, {recursive: true, force: true});
-fs.mkdirSync(DIST_LIB, {recursive: true});
+fs.rmSync(DIST_LIB, { recursive: true, force: true });
+fs.mkdirSync(DIST_LIB, { recursive: true });
 
 console.log(`Move font files from ${MODULE_FONT} to ${DIST_LIB_FONT}`);
 
--- test/part-exists.spec.ts
+++ test/part-exists.spec.ts
@@ -3,7 +3,8 @@ import fs from 'fs';
 
 const PART_DIR = path.resolve(__dirname, '..', 'part');
 
-const expectPartExists = (...files: string[]) => files.forEach((file) => expect(fs.existsSync(path.resolve(PART_DIR, file))).toBeTruthy());
+const expectPartExists = (...files: string[]) =>
+  files.forEach((file) => expect(fs.existsSync(path.resolve(PART_DIR, file))).toBeTruthy());
 
 describe('Part files exists', () => {
   it('Should part files exists', () => {
